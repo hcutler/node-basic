f9daefaa41ed618e6ec893e3d891419b983299f8
Brynn Claypoole <bclay@seas.upenn.edu>
2014-07-24 16:34:10 -0400

    cleaned html, added bootstrap

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var router = require("./routes/router.js");
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', router.launch);
 app.get('/ect', router.redirect);
 var port = Number(process.env.PORT || 5000);
